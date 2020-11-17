773bef7c628bdc809ad55c3968db18a455be72ae
Mishaker22 <michellq.hernandez@gmail.com>
2020-11-17 18:38:42 -0500

    subiendo bd

--- parcial2/utn.sql
+++ parcial2/utn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2020 a las 00:24:44
+-- Tiempo de generación: 18-11-2020 a las 00:37:11
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
